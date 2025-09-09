80c568aee0a6c437d53497ba861a97afbe92d9b3
alvarodv2 <alvarillo144@hotmail.com>
2025-09-09 13:35:41 +0200

    feat(entity): add Client entity with Lombok support
    - Added Client.java as JPA entity
    - Includes @Data, @NoArgsConstructor, and @AllArgsConstructor
    - Maps clientId, clientName, clientSecondName, and clientMembership fields
    
    Date: 2025-09-09

---
+++ src/main/java/com/spring_fitzone/entity/Client.java
@@ -0,0 +1,21 @@
+package com.spring_fitzone.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer clientId;
+    private String clientName;
+    private String clientSecondName;
+    private Integer clientMembership;
+
+}
